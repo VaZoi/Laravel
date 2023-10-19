a0919f1cb48424a907ccf0dea6420f02055f55d9
VZ <131747053+VaZoi@users.noreply.github.com>
2023-10-19 12:24:40 +0200

    m

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Collection;
 
-Route::get('/planets', function () {
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-
-    $Planet = request('planet');
-
-    if ($Planet) {
-        $planeten = collect($planets)->where('name', $Planet)->all();
-        return view('planets', ['planets' => $planeten]);
-    }
-
-    return view('planets', ['planets' => $planets]);
+Route::get('/planeten', function () {
+    $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return $planeten;
 });
 
 
