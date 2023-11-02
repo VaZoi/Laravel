15ab3ad477fb915f9c86c63394076f6e0e027a67
VZ <131747053+VaZoi@users.noreply.github.com>
2023-11-02 10:42:23 +0100

    f

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -3,41 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
+use App\Models\planets;
 
 class PlanetController extends Controller
 {
-    private $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-
     public function index()
     {
-        return view('planeten', ['planets' => $this->planets]);
+        $planets = planets::all();
+        return view('planeten', ['planets' => $planets]);
     }
 
     public function show($planet)
-{
-    $selectedPlanet = collect($this->planets)->where('name', $planet)->first();
-
-    return view('planeten', ['planets' => [$selectedPlanet]]);
+    {
+        $selectedPlanet = planets::where('name', $planet)->first();
+        return view('planet', ['planet' => $selectedPlanet]);
+    }
 }
 
-
-}
--- database/migrations/2023_11_02_090840_create_planets_table.php
+++ database/migrations/2023_11_02_090840_create_planets_table.php
@@ -11,7 +11,7 @@ class CreatePlanetsTable extends Migration
         Schema::create('planets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->integer('size_in_km');
             $table->timestamps();
         });
